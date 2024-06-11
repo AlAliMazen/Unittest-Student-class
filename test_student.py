3a5244a03a73cf942edcaec2b5622c76d60643be
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-11 20:43:31 +0200

    creating alert santa method and email method with tests

--- test_student.py
+++ test_student.py
@@ -8,6 +8,18 @@ class TestStudent(unittest.TestCase):
     def test_full_name(self):
         student = Student("John", "Doe")
         self.assertEqual(student.full_name, "John Doe")
+    
+    # second test will check when we call a method from student class called alert santa which should sit the attribute naughty to True
+    def test_alert_santa(self):
+        student = Student("John", "Doe")
+        student.alert_santa()
+        # we should see that the attribute should be turned to True when method is called
+        self.assertTrue(student.naughty_list)
+    
+    # test the email method if it return students'email address as firstname.lastname@email.compile
+    def test_student_email(self):
+        student = Student("John", "Doe")
+        self.assertEqual(student.email,"john.doe@email.com")
 
 
 
