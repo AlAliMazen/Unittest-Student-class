9200d7dade30b9530d6a547168edd4ca05cc8e21
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-11 18:59:18 +0200

    creating student class with construtor and some read only methode

---
+++ student.py
@@ -0,0 +1,23 @@
+# import the header for time and date
+from datetime import date, timedelta
+
+
+class Student:
+    """
+    A Student class as base for method testing
+    """
+    
+    def __init__(self, first_name, last_name):
+        
+        # we use underscore before the attribute to indicate it is read only function
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self.end_date = date.today()+timedelta(day=365) # this line doesn't take care of leap year
+        self.naughty_list = False
+
+    # in order to make this method a read only method , we have to add the decorative @ property
+    @property
+    def full_name(self):
+        return f"{self._first_name}  {self._last_name}"
+        
