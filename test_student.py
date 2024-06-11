5df93212edbcfcb219e32acc3d99e2d3be2f42df
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-11 21:12:20 +0200

    using code refactor by implementing setUp and tear Down methods

--- test_student.py
+++ test_student.py
@@ -3,23 +3,64 @@ from student import Student
 
 
 class TestStudent(unittest.TestCase):
+    """
+    whenever a unit test method is run, two python methods run before and after the test
+    both these functions must be written with camelCase and make use of the self keyword.
+
+    When looking at the test beneath, we can see that there is some kind of repeating
+    the code by the need of creating an instance of the class every time.
+
+    setUp method will be called before the test and 
+    tearDown method will be called after the test is done
+
+
+    there is also a possibility to call the function setUp and tearDown only once 
+    this use case is important for the creating of Database and populating it with 
+    data
+
+    in order to make use of this functionality we have to stick to the syntax
+    1- add a decorator of @classmethod 
+    2- add cls as parameter of both methods
+    3- use camelCase when writing method
+
+    """
+    @classmethod
+    def setUpClass(cls):
+        print("Set up class method")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("Tear down class method")
+
+        
+    def setUp(self):
+        print("Set Up method")
+        self.student=Student("John","Doe")
+    
+    def tearDown(self):
+        # id does nothing but destroy any objects and connections to the database
+        print("Tear Down method")
+
     # first we have to create an instance of student class inside the test function
     # first unittest for the function 
     def test_full_name(self):
-        student = Student("John", "Doe")
-        self.assertEqual(student.full_name, "John Doe")
+        print("test fullname method")
+        #student = Student("John", "Doe") # no need for this line anymore because we created the object in the setUp 
+        self.assertEqual(self.student.full_name, "John Doe")
     
     # second test will check when we call a method from student class called alert santa which should sit the attribute naughty to True
     def test_alert_santa(self):
-        student = Student("John", "Doe")
-        student.alert_santa()
+        print("test alert santa method")
+        #student = Student("John", "Doe")
+        self.student.alert_santa()
         # we should see that the attribute should be turned to True when method is called
-        self.assertTrue(student.naughty_list)
+        self.assertTrue(self.student.naughty_list)
     
     # test the email method if it return students'email address as firstname.lastname@email.compile
     def test_student_email(self):
-        student = Student("John", "Doe")
-        self.assertEqual(student.email,"john.doe@email.com")
+        print("test student mail")
+        #student = Student("John", "Doe")
+        self.assertEqual(self.student.email,"john.doe@email.com")
 
 
 
